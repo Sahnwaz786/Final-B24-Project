0ff6c8f49f6d71b73f7b1e0b18378aa2d5637b83
Sahnwaz786 <mdsahnwazalam48@gmail.com>
2023-08-03 16:36:12 +0530

    page4 completed

--- script.js
+++ script.js
@@ -40,3 +40,141 @@ gsap.to("#page1 svg",{
 
     }
 })
+
+
+
+ var h2Data = document.querySelectorAll("#page2  h2")
+
+h2Data.forEach(function(elem){
+    var textData = elem.textContent
+    var splitedText = textData.split("")
+
+    var clutter = ""
+
+   splitedText.forEach(function(e){
+   clutter += `<span>${e}</span>`
+   
+})
+   elem.innerHTML = clutter;
+})
+ 
+
+gsap.to("#page2  h2 span",{
+    color:"#E3E3C4",
+    stagger:0.2,
+    scrollTrigger:{
+        trigger:"#page2 h2 span",
+        scroller:"body",
+        // markers:true,
+        start:"top 50%",
+        end:"top -60%",
+        scrub:2
+    }
+})
+
+
+gsap.to("#page2  #svg1,page2 #svg2 ",{
+    left:"-20vw",
+    scrollTrigger:{
+        trigger:"#page2 #svg1",
+        scroller:"body",
+        // markers:true,
+        start:"top 55%",
+        end:"top -30%",
+        scrub:3
+    }
+})
+
+var b = document.querySelectorAll("#page3 h2")
+
+b.forEach(function(chacha){
+   var bData = chacha.textContent
+   var splitedB = bData.split("")
+
+   var clutter = ""
+
+   splitedB.forEach(function(chachi){
+    clutter += `<span>${chachi}</span>`
+   })
+   chacha.innerHTML = clutter
+})
+
+
+gsap.to("#page3  h2 span",{
+    // color:"#E3E3C4",
+    color:"#434B34",
+    stagger:0.2,
+    scrollTrigger:{
+        trigger:"#page3 h2 span",
+        scroller:"body",
+        // markers:true,
+        start:"top 60%",
+        end:"top 10%",
+        scrub:2
+    }
+})
+
+
+gsap.from("#page3  #bottom-part #part1>h5,#page3 #first-img ",{
+    opacity:0,
+    y:50,
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#page3 #bottom-part #part1>h5,#page3 #first-img ",
+        scroller:"body",
+        // markers:true,
+        start:"top 96%",
+        end:"top 75%",
+        scrub:1
+    }
+})
+
+
+gsap.from("#page3  #bottom-part #part1 #exp ",{
+    opacity:0,
+    y:50,
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#page3 #bottom-part #part1 #exp ",
+        scroller:"body",
+        // markers:true,
+        start:"top 97%",
+        end:"top 75%",
+        scrub:1
+    }
+})
+
+gsap.from("#page3  #bottom-part #part1>img ",{
+    opacity:0,
+    y:50,
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#page3 #bottom-part #part1>img ",
+        scroller:"body",
+        // markers:true,
+        start:"top 97%",
+        end:"top 75%",
+        scrub:1
+    }
+})
+
+gsap.from("#page3  #bottom-part #part2 #sec-img ",{
+    opacity:0,
+    y:50,
+    duration:0.5,
+    scrollTrigger:{
+        trigger:"#page3 #bottom-part #part2 #sec-img ",
+        scroller:"body",
+        // markers:true,
+        start:"top 97%",
+        end:"top 75%",
+        scrub:1
+    }
+})
+
+
+
+
+
+
+
